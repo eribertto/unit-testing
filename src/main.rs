26f9f7d97d0ab6e667411e10dd79e694297a3218
eribertt_fedorabox <erimendz@gmail.com>
2021-08-14 14:06:15 +0300

    add code for unit tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+// all about writing unit tests
+
 fn main() {
-    println!("Hello, world!");
+    println!("All about unit testing");
+	println!("2+2 = {}", add(2, 2));
+}
+
+fn add(a: i32, b: i32) -> i32 {
+	a + b	// no need for return keyword
+}
+
+#[test]
+fn add_works() {
+	assert_eq!(add(1, 2), 3);
+	assert_eq!(add(10, 12), 22);
+	assert_eq!(add(5, -2), 3);
+}
+
+#[test]
+#[should_panic]		// this attribute makes it possible to check for a panic
+fn add_fails() {
+	assert_eq!(add(2, 2), 7);
 }
