864dd9673cd29a2bb7ff49695c665bafa466d5a6
eribertt_fedorabox <erimendz@gmail.com>
2021-08-14 14:26:51 +0300

    create the test module and add all test functions

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,33 @@ fn add(a: i32, b: i32) -> i32 {
 	a + b	// no need for return keyword
 }
 
-#[test]
-fn add_works() {
-	assert_eq!(add(1, 2), 3);
-	assert_eq!(add(10, 12), 22);
-	assert_eq!(add(5, -2), 3);
-}
 
-#[test]
-#[should_panic]		// this attribute makes it possible to check for a panic
-fn add_fails() {
-	assert_eq!(add(2, 2), 7);
+// adding the test module
+// most unit tests go into a submodule with the #[cfg(test)] attribute
+// module name is given by the user
+// the cfg attribute controls conditional compilation and will only compile the thing it is attached to
+// if the predicate is true
+#[cfg(test)]
+mod add_function_tests {
+	use super::*;
+	#[test]
+	fn add_works() {
+		assert_eq!(add(1, 2), 3);
+		assert_eq!(add(10, 12), 22);
+		assert_eq!(add(5, -2), 3);
+	}
+
+	#[test]
+	#[should_panic]		// this attribute makes it possible to check for a panic
+	fn add_fails() {
+		assert_eq!(add(2, 2), 7);
+	}
+
+	#[test]
+	#[ignore = "not yet reviewed by the QA team"]
+	fn add_negatives() {
+		assert_eq!(add(-2, -2), -4);
+	}
+	// note ignored test functions will still be type checked and compiled but wont be executed
+
 }
